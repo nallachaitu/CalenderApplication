03f9c5b7a67a38e00181b282a69c9337834e5d07
nallagsoc <vivek.candid@gmail.com>
2014-10-25 07:36:30 -0400

    modified urls file

--- urls.py
+++ urls.py
@@ -11,5 +11,4 @@ urlpatterns = patterns('',
     url(r'^compose/(?P<recipient>[\+\w]+)/$', compose, name='messages_compose_to'),
     url(r'^reply/(?P<message_id>[\d]+)/$', reply, name='messages_reply'),
     url(r'^view/(?P<message_id>[\d]+)/$', view, name='messages_detail'),
-    url(r'^delete/(?P<message_id>[\d]+)/$', delete, name='messages_delete'),
-    )
+        )
